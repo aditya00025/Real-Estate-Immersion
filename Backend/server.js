87e999be527b7d663a9387911a0dd7f70bf9072f
Chinmay Katdare <chinmayy05@gmail.com>
2025-02-17 15:41:47 +0530

    tried to upload image onto storage and image url into firestore

--- Backend/server.js
+++ Backend/server.js
@@ -1,14 +1,15 @@
-const express = require("express");
-const cors = require("cors");
-const appdata = require("./Appdata"); // Import the route module
+const express = require('express');
+const cors = require('cors');
+const appdata = require('./Appdata'); // Import the route module
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 // Use the appdata routes; the /submit-form route will be available at http://localhost:5000/submit-form
-app.use("/", appdata);
+app.use('/', appdata);
 
 const PORT = 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
-
+app.listen(PORT, () =>
+  console.log(`Server running on http://localhost:${PORT}`)
+);
